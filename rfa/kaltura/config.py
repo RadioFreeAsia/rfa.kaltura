93ae62e2ce860444fdc8be1f3905828ca3d69659
Flip McFadden <flipmcf@gmail.com>
2013-07-22 17:14:18 -0700

    adding kaltura config stuff to config

--- rfa/kaltura/config.py
+++ rfa/kaltura/config.py
@@ -7,3 +7,17 @@ ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
     'KalturaVideo': 'rfa.kaltura: Add Kaltura Video',
 }
+
+
+#Kaltura Account Information
+#kaltura_secret_config should be set as svn:ignore in your repo - just a suggestion
+try:
+    from kaltura_secret_config import PARTNER_ID, SECRET, ADMIN_SECRET, SERVICE_URL, USER_NAME
+except ImportError:
+    # UPDATE THIS
+    PARTNER_ID = 54321
+    SECRET = "YOUR_USER_SECRET"
+    ADMIN_SECRET = "YOUR_ADMIN_SECRET"
+    SERVICE_URL = "http://www.kaltura.com"
+    USER_NAME = "testUser"
+
