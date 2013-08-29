e6c7bfb7b171903ccadd1506ad8f6b58c7cf36b9
flipmcf <flipmcf@gmail.com>
2013-08-29 18:13:02 -0400

    Templates for preview and meta view

--- rfa/kaltura/browser/kaltura_video.py
+++ rfa/kaltura/browser/kaltura_video.py
@@ -3,22 +3,17 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class VideoView(BrowserView):
     
-    template = ViewPageTemplateFile('../skins/default/KalturaVideo.pt')
+    template = ViewPageTemplateFile('../skins/default/VideoView.pt')
     
     def __call__(self):
         """
         """
-        
-        self.info = {'something': "Video Player",
-                     'another': "Video Player",
-                     'testing': "you bet!"
-                     }
         return self.template()
     
 
 class MetaView(BrowserView):
     
-    template = ViewPageTemplateFile('../skins/default/KalturaVideo.pt')
+    template = ViewPageTemplateFile('../skins/default/VideoMeta.pt')
     
     def __call__(self):
         """
