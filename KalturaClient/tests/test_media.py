acf6634d1d626b2682562c3b812c75c5e21e2aea
flipmcf <flipmcf@gmail.com>
2013-09-27 19:21:50 -0400

    refactor test for better design

--- KalturaClient/tests/test_media.py
+++ KalturaClient/tests/test_media.py
@@ -2,33 +2,35 @@ from utils import GetConfig
 from utils import KalturaBaseTest
 from utils import getTestFile
 
-import KalturaClient.Plugins.Core as KalturaCoreClient
-
+from KalturaClient.Plugins.Core import KalturaMediaListResponse
+from KalturaClient.Plugins.Core import KalturaMediaEntry, KalturaMediaType
 
 
 class MediaTests(KalturaBaseTest):
     
     def test_list(self):
         resp = self.client.media.list()
-        self.assertIsInstance(resp, KalturaCoreClient.KalturaMediaListResponse)
+        self.assertIsInstance(resp, KalturaMediaListResponse)
         
         objs = resp.objects
         self.assertIsInstance(objs, list)
         
-        [self.assertIsInstance(o, KalturaCoreClient.KalturaMediaEntry) for o in objs]
+        [self.assertIsInstance(o, KalturaMediaEntry) for o in objs]
         
 
     def test_createRemote(self):
-        mediaEntry = KalturaCoreClient.KalturaMediaEntry()
+        mediaEntry = KalturaMediaEntry()
         mediaEntry.setName('pytest.MediaTests.test_createRemote')
-        mediaEntry.setMediaType(KalturaCoreClient.KalturaMediaType(KalturaCoreClient.KalturaMediaType.VIDEO))
+        mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
             
         ulFile = getTestFile('DemoVideo.flv')
         uploadTokenId = self.client.media.upload(ulFile)            
                      
         mediaEntry = self.client.media.addFromUploadedFile(mediaEntry, uploadTokenId)
         
-        #delete it
+        self.assertIsInstance(mediaEntry.getId(), unicode)
+        
+        #cleanup
         self.client.media.delete(mediaEntry.id)
         
 
