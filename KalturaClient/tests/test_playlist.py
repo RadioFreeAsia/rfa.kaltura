5f71d72b83e6df83b76b4a934c76b59e4b7921a3
flipmcf <flipmcf@gmail.com>
2013-10-03 17:10:01 -0400

    more playlist tests.  Some fail.  Trying to figure out why

--- KalturaClient/tests/test_playlist.py
+++ KalturaClient/tests/test_playlist.py
@@ -40,12 +40,31 @@ class PlaylistTests(KalturaBaseTest):
     #    assertIsInstance(kplaylist.playlistContent.split(','), list)
         
         
-    def test_addStaticContent(self):        
+        
+    def test_update(self):
+        kplaylist = KalturaPlaylist()
+        kplaylist.setName('pytest.PlaylistTests.test_update')        
+        kplaylist.setPlaylistType(KalturaPlaylistType(KalturaPlaylistType.STATIC_LIST))
+        kplaylist = self.client.playlist.add(kplaylist)
+        
+        
+        self.addCleanup(self.client.playlist.delete, kplaylist.getId())
+        
+        remotePlaylist = self.client.playlist.get(kplaylist.getId())
+        
+        #bug? I have to set this now.
+        remotePlaylist.setReferenceId("pytest.PlaylistTests.test_update")
+        
+        
+        #no change, just update
+        self.client.playlist.update(kplaylist.getId(), remotePlaylist)
+        
+    def test_updateStaticContent(self):        
                 
         from KalturaClient.Plugins.Core import KalturaMediaEntry, KalturaMediaType
         
         mediaEntry1 = KalturaMediaEntry()
-        mediaEntry1.setName('pytest.PlaylistTests.test_createStaticContent1')
+        mediaEntry1.setName('pytest.PlaylistTests.test_updateStaticContent1')
         mediaEntry1.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
         ulFile = getTestFile('DemoVideo.flv')
         uploadTokenId = self.client.media.upload(ulFile) 
@@ -54,7 +73,7 @@ class PlaylistTests(KalturaBaseTest):
         self.addCleanup(self.client.media.delete, mediaEntry1.getId())
                 
         mediaEntry2 = KalturaMediaEntry()
-        mediaEntry2.setName('pytest.PlaylistTests.test_createStaticContent2')
+        mediaEntry2.setName('pytest.PlaylistTests.test_updateStaticContent2')
         mediaEntry2.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
         ulFile = getTestFile('DemoVideo.flv')
         uploadTokenId = self.client.media.upload(ulFile) 
@@ -66,8 +85,8 @@ class PlaylistTests(KalturaBaseTest):
         playlistContent = u','.join([mediaEntry1.getId(), mediaEntry2.getId()])
                         
         kplaylist = KalturaPlaylist()
-        kplaylist.setName('pytest.PlaylistTests.test_createStaticContent')
-        kplaylist.setPlaylistType(KalturaPlaylistType(KalturaPlaylistType.STATIC_LIST)) #??? STATIC LIST ???        
+        kplaylist.setName('pytest.PlaylistTests.test_updateStaticContent')
+        kplaylist.setPlaylistType(KalturaPlaylistType(KalturaPlaylistType.STATIC_LIST))
         
         kplaylist.playlistContent = playlistContent        
         kplaylist = self.client.playlist.add(kplaylist)
@@ -81,7 +100,66 @@ class PlaylistTests(KalturaBaseTest):
         #import pdb; pdb.set_trace()  #go check your server
         
         
-
+    def test_addStaticToExistingEmpty(self):
+        from KalturaClient.Plugins.Core import KalturaMediaEntry, KalturaMediaType
+        
+        import pdb; pdb.set_trace()
+        #create empty playlist on server
+        kplaylist = KalturaPlaylist()
+        kplaylist.setName('pytest.PlaylistTests.test_addStaticToExistingEmpty')
+        kplaylist.setPlaylistType(KalturaPlaylistType(KalturaPlaylistType.STATIC_LIST))
+        kplaylist = self.client.playlist.add(kplaylist)
+        self.addCleanup(self.client.playlist.delete, kplaylist.getId())
+        
+        playlistId = kplaylist.getId()
+        
+        #now, add some media
+        
+        mediaEntry = KalturaMediaEntry()
+        mediaEntry.setName('pytest.PlaylistTests.test_addStaticToExistingEmpty')
+        mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
+        ulFile = getTestFile('DemoVideo.flv')
+        uploadTokenId = self.client.media.upload(ulFile) 
+        mediaEntry = self.client.media.addFromUploadedFile(mediaEntry, uploadTokenId)         
+        self.addCleanup(self.client.media.delete, mediaEntry.getId())
+        
+        #add to (update) existing playlist
+        playlist = self.client.playlist.get(playlistId)
+        
+        playlistContent = u','.join([mediaEntry.getId()])
+        
+        playlist.setPlaylistContent(playlistContent)
+        playlist.referenceId = "pytest.PlaylistTests.test_addStaticToExistingEmpty"
+        import pdb; pdb.set_trace()
+        playlist.conversionProfileId = ''
+        self.client.playlist.update(playlistId, playlist)
+        del(playlist)
+        
+        #check it.
+        playlist = self.client.playlist.get(playlistId)
+        
+        self.assertEqual(playlistContent, playlist.playlistContent)
+        
+        
+        
+    def test_updateExceptionReferenceIdNotSet(self):
+        from KalturaClient.Base import KalturaException
+        
+        kplaylist = KalturaPlaylist()
+        kplaylist.setName('pytest.PlaylistTests.test_updateExceptionReferenceIdNotSet')
+        kplaylist.setPlaylistType(KalturaPlaylistType(KalturaPlaylistType.STATIC_LIST))
+        kplaylist = self.client.playlist.add(kplaylist)
+        self.addCleanup(self.client.playlist.delete, kplaylist.getId())
+        
+        playlistId = kplaylist.getId()
+        
+        playlist = self.client.playlist.get(playlistId)
+        
+        #don't set referenceId
+        
+        self.assertRaises(KalturaException, self.client.playlist.update, playlistId, playlist) 
+        
+        
 import unittest
 def test_suite():
     return unittest.TestSuite((
