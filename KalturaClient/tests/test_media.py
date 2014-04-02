d64e3db16dbac123bb30a369163c34e270cd1ee2
flipmcf <flipmcf@gmail.com>
2014-04-02 16:01:15 -0400

    moving tests to new utf8 test class - add test_utf8_name (fails)

--- KalturaClient/tests/test_media.py
+++ KalturaClient/tests/test_media.py
@@ -35,7 +35,22 @@ class MediaTests(KalturaBaseTest):
         #cleanup
         self.client.media.delete(mediaEntry.id)
         
-    def test_set_UTF8_tags(self):
+class Utf8_tests(KalturaBaseTest):
+    
+    def test_utf8_name(self):
+        test_unicode = u'\u03dd\xf5\xf6'  #an odd representation of the word 'FOO'
+        mediaEntry = KalturaMediaEntry()
+        mediaEntry.setName(u'pytest.MediaTests.test_UTF8_tags'+test_unicode)
+        mediaEntry.setMediaType(KalturaMediaType(KalturaMediaType.VIDEO))
+        ulFile = getTestFile('DemoVideo.flv')
+        uploadTokenId = self.client.media.upload(ulFile)
+        
+        #this will throw an exception if fail.
+        mediaEntry = self.client.media.addFromUploadedFile(mediaEntry, uploadTokenId)
+            
+        self.addCleanup(self.client.media.delete, mediaEntry.getId())
+    
+    def test_utf8_tags(self):
 
         test_unicode = u'\u03dd\xf5\xf6'  #an odd representation of the word 'FOO'
         mediaEntry = KalturaMediaEntry()
@@ -55,6 +70,7 @@ class MediaTests(KalturaBaseTest):
 def test_suite():
     return unittest.TestSuite((
         unittest.makeSuite(MediaTests),
+        unittest.makeSuite(Utf8_tests)
         ))
 
 if __name__ == "__main__":
