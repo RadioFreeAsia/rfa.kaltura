1419cae8bcd74ac9639559737607730ad47e0ee6
flipmcf <flipmcf@gmail.com>
2014-05-23 17:17:55 -0400

    half-cooked idea on how to do edits from plone on metadata

--- rfa/kaltura/events/events.py
+++ rfa/kaltura/events/events.py
@@ -4,6 +4,7 @@ from rfa.kaltura.interfaces import IKalturaPlaylist
 
 from rfa.kaltura.kutils import KalturaLoggerInstance as logger
 from rfa.kaltura.kutils import kupload, kcreatePlaylist, kcreateVideo
+from rfa.kaltura.kutils import kdiff
 
 def initVideo(context, event):
     """Fired when the object is first populated"""
@@ -17,7 +18,22 @@ def initVideo(context, event):
         
 def modifyVideo(context, event):
     """Fired when the object is edited"""
+    import pdb; pdb.set_trace()
     
+    changed_fields = kdiff(context, context.KalturaObject)
+
+    if changed_fields:
+        kwargs = {}
+        for (pfield, kfield) in changed_fields:
+            val = getattr(context, pfield)
+            if callable(val):
+                val = val()
+           
+            kwargs[kfield] = value
+        context.updateRemote(**kwargs)
+            
+        
+    #has video file changed?
     status = context.REQUEST.form.get('file_delete')
     if status in ("nochange", None):
         pass #not modified
