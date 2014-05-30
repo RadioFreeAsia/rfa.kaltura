83a0958c7c1d420069e11dbaef89739e845e0c68
flipmcf <flipmcf@gmail.com>
2014-05-30 15:16:27 -0400

    add TopLevelCategory setting to getCategories vocabulary - edit pages now filtered

--- rfa/kaltura/content/vocabularies.py
+++ rfa/kaltura/content/vocabularies.py
@@ -1,10 +1,11 @@
 """functions for populating vocabularies for various select or multiselect fields"""
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 
-from rfa.kaltura.kutils import kGetPlaylistPlayers
-from rfa.kaltura.kutils import kGetCategories
-from rfa.kaltura.kutils import kGetVideoPlayers
-
+from rfa.kaltura.controlpanel import IRfaKalturaSettings
+from rfa.kaltura.kutils import kGetPlaylistPlayers, kGetVideoPlayers
+from rfa.kaltura.kutils import kGetCategories, kGetCategoryId
 from rfa.kaltura.interfaces import IKalturaVideo
 from rfa.kaltura.interfaces import IKalturaPlaylist
 
@@ -15,8 +16,18 @@ def getTagVocabulary():
 
 def getCategoryVocabulary(parent=None):
     """Get Currently created Categories on Kaltura server"""
-    items = []
     
+    items = []
+    if parent is None:
+        #check settings for 'top level category'
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IRfaKalturaSettings)
+        tlc = settings.topLevelCategory
+        if tlc:
+            parent = kGetCategoryId(categoryName=tlc)
+            #place the top-level category at the top of the list, so it's selectable.
+            items.append( (str(parent), tlc,) )
+        
     categoryObjs = kGetCategories(parent)
     for cat in categoryObjs:
         items.append( (str(cat.getId()), cat.getName(),) )
