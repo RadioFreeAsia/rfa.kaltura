831245669d49790beb1d989b63a5ece28479681f
flipmcf <flipmcf@gmail.com>
2014-11-19 16:58:39 -0500

    Some test case work

---
+++ rfa/kaltura/tests/_test_content.py
@@ -0,0 +1,7 @@
+"""content tests"""
+
+#important tests to write
+# content.kalturavideo.KalturaVideo.updateCategories
+# content.kalturavideo.KalturaVideo.updateTags
+# content.kalturavideo.KalturaVideo.updateRemote
+
